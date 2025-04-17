619b351dc600f46eafcdca0d68229b528aa30721
SBINNONNA <karimessimax@gmail.com>
2025-04-17 15:00:10 +0200

    commit

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,11 +1,8 @@
-import P_Aeroporto.Volo;
-import P_Aeroporto.VoloDestinazione;
-import P_Aeroporto.Volo_Origine;
 
 public class Main {
     public static void main(String[] args) {
 
-       //     Volo v = new Volo();
+       Volo v = new Volo();
 
 
     }
--- src/src/P_Aeroporto/VoloOrigine.java
+++ src/src/P_Aeroporto/VoloOrigine.java
@@ -1,9 +1,9 @@
 package P_Aeroporto;
 
-public class Volo_Origine extends Volo {
+public class VoloOrigine extends Volo {
     private Gate imbarco;
 
-    public Volo_Origine(Gate imbarco, Volo v) {
+    public VoloOrigine(Gate imbarco, Volo v) {
         super();
         v.setA_Volo_Origine("NAPOLI");
         this.imbarco = imbarco;
